7d66701a2cd5f3949dafa67365f47b27cfb0c509
8f00ff (Vi) <8f00ff@proton.me>
2025-06-11 01:49:17 -0400

    testing separate downloads

--- engines/uqm-megamod/build.sh
+++ engines/uqm-megamod/build.sh
@@ -6,10 +6,6 @@ pushd source
 git checkout "$COMMIT_TAG"
 popd
 
-for i in ${UQM_PACKAGES[@]} ${UQM_ADDONS[@]}; do
-  curl -sLO "https://gigenet.dl.sourceforge.net/project/uqm-mods/MegaMod/${COMMIT_TAG}/Content/${i}"
-done
-
 # BUILD PHASE
 cp -rfv ./config.state source
 pushd "source"
@@ -21,17 +17,5 @@ popd
 # COPY PHASE
 cp -v source/UrQuanMasters "${diststart}/2645580/dist/uqm-megamod"
 cp -v assets/uqm-megamod.sh "${diststart}/2645580/dist/uqm-megamod.sh"
-
-mkdir -p "${diststart}/2645580/dist/megamod/content/packages"
-for i in ${UQM_PACKAGES[@]}; do
-  cp -rvf "${i}" "${diststart}/2645580/dist/megamod/content/packages/${i}"
-done
-
-mkdir -p "${diststart}/2645580/dist/megamod/content/addons"
-for i in ${UQM_ADDONS[@]}; do
-  cp -rvf "${i}" "${diststart}/2645580/dist/megamod/content/addons/${i}"
-done
-
+mkdir -p "${diststart}/2645580/dist/megamod/content/packages" "${diststart}/2645580/dist/megamod/content/addons"
 echo -e "${COMMIT_TAG}"'\n\n$Format:%ad$' > "${diststart}/2645580/dist/megamod/content/version"
-
-unset UQM_PACKAGES UQM_ADDONS
